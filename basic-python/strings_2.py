427926de043d35fc392e0059bc7b438afe10f683
nely de faria <nelly.nf54@gmail.com>
2022-03-07 20:06:56 -0300

    Criando arquivo strings_2.py

---
+++ basic-python/strings_2.py
@@ -0,0 +1,12 @@
+cumprimento = 'Olá, '
+nome = 'Felipe'
+print(cumprimento + nome)
+print(nome * 5)
+
+name = 'Felipe'
+age = 35
+n_kids = 2
+print(f'{name} tem {age} e {n_kids} filhos.')
+
+preco_gasolina = 3.476
+print('o preço da gasolina hoje subiu e está em R$ {:.2f}' .format(preco_gasolina))
