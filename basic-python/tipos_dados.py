e64de9546c1883b3dc3f0122ea836670ac63de8c
nely de faria <nelly.nf54@gmail.com>
2022-03-07 13:19:34 -0300

    Criando arquivo tipos_dados.py

---
+++ basic-python/tipos_dados.py
@@ -0,0 +1,12 @@
+x = 5
+print(x)
+print(type(x))
+
+preco = 19.90
+print(preco, type(preco))
+
+cidade = 'São Paulo'
+print(cidade, type(cidade))
+
+disponivel = True
+print(disponivel, type(disponivel))
