4a081589b496a94cc973c42f7eaee7603c4af952
nely de faria <nelly.nf54@gmail.com>
2022-03-09 13:44:27 -0300

    Criando arquivo api.py

---
+++ basic-python/api.py
@@ -0,0 +1,12 @@
+import requests
+
+url = 'https://api.exchangerate-api.com/v6/latest'
+
+req = requests.get(url)
+
+# print(req.status_code)
+dados = req.json()
+# print(dados)
+valor_reais = float(input('informe o valor em R$ a ser convertido\n'))
+cotacao = dados['rates']['BRL']
+print(f'R${valor_reais} em dólar valem US$ {(valor_reais / cotacao):.2f}')
